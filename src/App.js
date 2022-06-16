9bc7d8dbf7608da1f4e71ccc80eabc7505dfcc17
Gonzalo León <gonzaloleon_chavez@hotmail.com>
2022-06-16 08:24:52 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Carrucel from './component/carrucel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Carrucel/>
+    </div>
+  );
+}
+
+export default App;
